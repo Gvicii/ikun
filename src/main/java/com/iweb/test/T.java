12d8bce97536bd74936395a2dc3cdb6c7f5fe21b
Guan <635219677@qq.com>
2024-07-23 16:33:02 +0800

    代码的每一次提交都会生成一个版本 提示信息是用来给别人看的 你需要告诉团队成员你修改了那些代码 优化了哪些功能

---
+++ src/main/java/com/iweb/test/T.java
@@ -0,0 +1,10 @@
+package com.iweb.test;
+
+/**
+ * @author G
+ * @date 2024/7/23 11:51
+ */
+
+
+public class T {
+}
---
+++ src/main/java/com/iweb/test/TestStudentDAO.java
@@ -0,0 +1,35 @@
+package com.iweb.test;
+
+import com.iweb.dao.StudentDAO;
+import com.iweb.dao.impl.StudentDAOImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/** 测试类 用于DAO接口测试
+ * @Test 将指定方法标记为测试方法 可以不依赖于main方法直接运行
+ * @Before 在所有测试方法运行之前执行的代码 一般用于环境的初始化
+ * @After 在所有测试方法运行之后执行的代码 一般用于资源回收
+ * @author G
+ * @date 2024/7/23 11:01
+ */
+public class TestStudentDAO {
+    private StudentDAO studentDAO;
+    @Before
+    public void init(){
+        studentDAO = new StudentDAOImpl();
+    }
+    @Test
+    public void testFindById(){
+        Assert.assertNotNull(studentDAO.findById(1));
+//        Assert.assertNotNull(null);
+    }
+    @Test
+    public void testCount(){
+        Assert.assertEquals(9L,(long)studentDAO.count());
+    }
+    @Test
+    public void testFindAll(){
+        Assert.assertNotNull(studentDAO.findAll());
+    }
+}
